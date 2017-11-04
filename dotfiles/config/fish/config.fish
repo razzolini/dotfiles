aa274fc3f2c74cbca4d19707a086f444d9729c8a
Riccardo Azzolini <riccardo4096@gmail.com>
2017-11-04 13:35:35 +0100

    Change dirty state character in fish git prompt because of width issues.

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -12,7 +12,7 @@ set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 # Chars
-set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_dirtystate 'Δ'
 set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_untrackedfiles '☡'
 set __fish_git_prompt_char_stashstate '↩'
