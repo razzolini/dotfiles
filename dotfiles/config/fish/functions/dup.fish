354e61d647632aeaeffbea7165757558f7e00c41
Riccardo Azzolini <riccardo4096@gmail.com>
2018-09-02 13:00:23 +0200

    Remove unneeded CWD argument in fish `dup` function.

--- dotfiles/config/fish/functions/dup.fish
+++ dotfiles/config/fish/functions/dup.fish
@@ -1,4 +1,4 @@
 function dup --description "Open another terminal window with the same working directory"
-    xterm -e fish --init-command 'cd '(string escape $PWD) &
+    xterm -e fish &
     disown
 end
