fe845453c08b5af4c12b272d5244bb1d5f6d0990
Riccardo Azzolini <riccardo4096@gmail.com>
2019-01-11 11:25:03 +0100

    Remove special casing of PATH variables in import_bash_env.fish
    Since version 3.0, fish treats all variables with names ending in PATH
    as colon-separated, and splits them automatically on assignment.
    See https://github.com/fish-shell/fish-shell/pull/5245

--- dotfiles/config/fish/functions/import_bash_env.fish
+++ dotfiles/config/fish/functions/import_bash_env.fish
@@ -1,8 +1,6 @@
 function import_bash_env --description "Import environment variables from bash"
     # Variables which should not be imported
     set -l excluded PWD OLDPWD SHLVL
-    # Special colon-separated variables which fish treats as lists
-    set -l lists PATH CDPATH MANPATH
 
     env --ignore-environment HOME=$HOME bash --login -c 'export -p' \
     | while read -l var
@@ -12,10 +10,6 @@ function import_bash_env --description "Import environment variables from bash"
 
         if contains $name $excluded
             continue
-        end
-
-        if contains $name $lists
-            set value (string split ':' $value)
         end
 
         set --global --export $name $value
