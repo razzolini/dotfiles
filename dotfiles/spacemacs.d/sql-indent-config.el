7fb9c399ec466a53623046cfc9a2154e078bbcaa
Riccardo Azzolini <riccardo4096@gmail.com>
2019-10-15 11:24:35 +0200

    Indent by a fixed amount inside parens

--- dotfiles/spacemacs.d/sql-indent-config.el
+++ dotfiles/spacemacs.d/sql-indent-config.el
@@ -3,7 +3,10 @@
   (require 'sql-indent-left)
   (setq
    sqlind-basic-offset 2
-   sqlind-indentation-offsets-alist `((in-select-clause +)
-                                      (select-join-condition +)
-                                      ,@sqlind-indentation-left-offsets-alist)))
+   sqlind-indentation-offsets-alist
+   `((in-select-clause +)
+     (select-join-condition +)
+     (nested-statement-open sqlind-use-anchor-indentation +)
+     (nested-statement-continuation sqlind-use-anchor-indentation +)
+     ,@sqlind-indentation-left-offsets-alist)))
 (add-hook 'sqlind-minor-mode-hook 'configure-sql-indentation)
