dc3bdb91bc54050627e7318b21d86c15d3deff77
Riccardo Azzolini <riccardo4096@gmail.com>
2019-10-15 11:06:08 +0200

    Reduce JOIN condition indentation

--- dotfiles/spacemacs.d/sql-indent-config.el
+++ dotfiles/spacemacs.d/sql-indent-config.el
@@ -4,5 +4,6 @@
   (setq
    sqlind-basic-offset 2
    sqlind-indentation-offsets-alist `((in-select-clause +)
+                                      (select-join-condition +)
                                       ,@sqlind-indentation-left-offsets-alist)))
 (add-hook 'sqlind-minor-mode-hook 'configure-sql-indentation)
