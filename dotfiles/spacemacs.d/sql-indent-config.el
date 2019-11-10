0cffce600ebcda2a8a96ea25dc5b2c68a2326697
Riccardo Azzolini <riccardo4096@gmail.com>
2019-11-10 18:52:56 +0100

    Indent continuations of UPDATE clauses

--- dotfiles/spacemacs.d/sql-indent-config.el
+++ dotfiles/spacemacs.d/sql-indent-config.el
@@ -9,5 +9,6 @@
      (nested-statement-open sqlind-use-anchor-indentation +)
      (nested-statement-continuation sqlind-use-anchor-indentation +)
      (create-statement 0) ;; For ALTER TABLE actions
+     (in-update-clause +)
      ,@sqlind-indentation-left-offsets-alist)))
 (add-hook 'sqlind-minor-mode-hook 'configure-sql-indentation)
