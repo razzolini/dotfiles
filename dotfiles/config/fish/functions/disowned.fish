c9ee318bdc53d923f045c0a7f21ee4cb0a8575b5
Riccardo Azzolini <riccardo4096@gmail.com>
2020-03-01 18:38:58 +0100

    fish: Remove deprecated caret redirection in `disowned' function
    The `disowned' function still used the caret, `^', for stderr redirection,
    despite it having been deprecated since fish 3.0.
    Replace it with the new `&>' syntax, which redirects both stdout and stderr.

--- dotfiles/config/fish/functions/disowned.fish
+++ dotfiles/config/fish/functions/disowned.fish
@@ -3,7 +3,7 @@ function disowned --description 'Run a command in the background with suppressed
     # '&' must be part of the evaluated string, otherwise eval itself is run in
     # the background and the disown command is executed before the job is
     # created, therefore it acts on the wrong process.
-    eval "$escaped_argv &" </dev/null >/dev/null ^&1
+    eval "$escaped_argv &" </dev/null &>/dev/null
     disown
 end
 
