42b730e11272f74d0b39d4872686319a30be95a7
Riccardo Azzolini <riccardo4096@gmail.com>
2020-06-21 14:22:29 +0200

    Set urxvt and fish colors explicitly
    This commit doesn't actually change any settings, but it sets up for
    better tracking of future changes.
    Keeping track of previous settings is especially important for fish
    color variables, which are universal (automatically persisted on disk
    every time they're set), therefore removing `set' commands from the
    config is not sufficient to restore default values.

---
+++ dotfiles/config/fish/conf.d/theme.fish
@@ -0,0 +1,31 @@
+# Fish color theme
+# See https://fishshell.com/docs/current/#variables-color for a detailed
+# explanation of these variables.
+
+set fish_color_autosuggestion 555
+set fish_color_cancel -r
+set fish_color_command 005fd7
+set fish_color_comment 990000
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end 009900
+set fish_color_error ff0000
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host normal
+set fish_color_host_remote yellow
+set fish_color_match cyan
+set fish_color_normal normal
+set fish_color_operator cyan
+set fish_color_param 00afff
+set fish_color_quote 999900
+set fish_color_redirection 00afff
+set fish_color_search_match --background=purple
+set fish_color_selection --background=purple
+set fish_color_status red
+set fish_color_user green
+set fish_color_valid_path --underline
+set fish_pager_color_completion normal
+set fish_pager_color_description 555 yellow
+set fish_pager_color_prefix cyan
+set fish_pager_color_progress cyan
