676fe3a03a1206fb3e6141f187ade3a216e84e36
Riccardo Azzolini <riccardo4096@gmail.com>
2022-10-27 18:15:13 +0200

    Add Firefox user.js preferences file

---
+++ dotfiles/firefox/user.js
@@ -0,0 +1,5 @@
+// Respect system scroll wheel behavior (otherwise, Firefox tends to scroll
+// much faster than other applications).
+user_pref("mousewheel.system_scroll_override.enabled", false);
+// Disable F11 full screen animation (and others).
+user_pref("ui.prefersReducedMotion", 1);
