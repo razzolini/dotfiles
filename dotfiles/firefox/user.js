c5d0f776a8966c7c44c07ad6f63b8546efcd6886
Riccardo Azzolini <11586658+razzolini@users.noreply.github.com>
2022-11-21 16:39:31 +0100

    firefox: Disable microphone/webcam indicator

--- dotfiles/firefox/user.js
+++ dotfiles/firefox/user.js
@@ -1,5 +1,7 @@
 // Respect system scroll wheel behavior (otherwise, Firefox tends to scroll
 // much faster than other applications).
 user_pref("mousewheel.system_scroll_override.enabled", false);
+// Disable microphone/webcam indicator overlay.
+user_pref("privacy.webrtc.legacyGlobalIndicator", false);
 // Disable F11 full screen animation (and others).
 user_pref("ui.prefersReducedMotion", 1);
